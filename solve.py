c0947579d38a8145bc8379bcce6e98777bf7cfd4
Amit Fridman <amit.fridman@opsys-tech.com>
2022-04-03 17:18:04 +0300

    Sort candidates by the occurence of new letters

--- solve.py
+++ solve.py
@@ -1,7 +1,36 @@
 tries = [
-    ('TRAIN', 'o    '),
-    ('COMET', 'xo  o')
+    ('TEARS', 'oo  o'),
+    ('SMITE', 'x  ox')
 ]
+
+occurence = {
+    'E': 11.1607,
+    'M': 3.0129,
+    'A': 8.4966,
+    'H': 3.0034,
+    'R': 7.5809,
+    'G': 2.4705,
+    'I': 7.5448,
+    'B': 2.0720,
+    'O': 7.1635,
+    'F': 1.8121,
+    'T': 6.9509,
+    'Y': 1.7779,
+    'N': 6.6544,
+    'W': 1.2899,
+    'S': 5.7351,
+    'K': 1.1016,
+    'L': 5.4893,
+    'V': 1.0074,
+    'C': 4.5388,
+    'X': 0.2902,
+    'U': 3.6308,
+    'Z': 0.2722,
+    'D': 3.3844,
+    'J': 0.1965,
+    'P': 3.1671,
+    'Q': 0.1962
+}
 
 knowledge = []
 for line in tries:
@@ -17,6 +46,7 @@ with open('valid_words.txt') as word_file:
     print(len(words))
     with open('options.txt', 'w') as options_file:
         count = 0
+        options = []
         for word in words:
             fail = False
             for know in knowledge:
@@ -34,6 +64,19 @@ with open('valid_words.txt') as word_file:
                         if word[i] == letter or word.find(letter) < 0:
                             fail = True
             if not fail:
-                options_file.write(word + '\n')
+                seen = [False, False, False, False, False]
+                for line in tries:
+                    for i in range(5):
+                        if line[0].find(word[i]) >= 0:
+                            seen[i] = True
+                occ = 0.0
+                print(seen)
+                for i in range(5):
+                    if not seen[i]:
+                        occ += occurence[word[i]]
+                options.append((word, occ))
                 count += 1
+        options.sort(key=lambda tuple: tuple[1], reverse=True)
+        for option in options:
+            options_file.write("%s\n" % option[0])
 print(count)
